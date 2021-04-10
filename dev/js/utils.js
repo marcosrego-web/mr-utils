d7adb03c480444ff6e1f7a6372d1f061b4a24acd
Marcos Rego <web@marcosrego.com>
2021-04-10 15:32:48 +0100

    Horizontal Scroll and Swipe

--- dev/js/utils.js
+++ dev/js/utils.js
@@ -145,3 +145,35 @@ document.addEventListener("click", function (t) {
           .querySelector(".mr-offcanvas")
           .classList.add("mr-transitionbottom")));
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const eles = document.querySelectorAll(".mr-grab,.mr-swipe");
+  for (id = 0; id < eles.length; id++) {
+    const ele = eles[id];
+    ele.classList.remove("mr-grabbing");
+    let pos = { top: 0, left: 0, x: 0, y: 0 };
+    const mouseDownHandler = function (e) {
+      ele.classList.add("mr-grabbing");
+      pos = {
+        left: ele.scrollLeft,
+        top: ele.scrollTop,
+        x: e.clientX,
+        y: e.clientY,
+      };
+      document.addEventListener("mousemove", mouseMoveHandler);
+      document.addEventListener("mouseup", mouseUpHandler);
+    };
+    const mouseMoveHandler = function (e) {
+      const dx = e.clientX - pos.x;
+      const dy = e.clientY - pos.y;
+      ele.scrollTop = pos.top - dy;
+      ele.scrollLeft = pos.left - dx;
+    };
+    const mouseUpHandler = function () {
+      ele.classList.remove("mr-grabbing");
+      ele.style.removeProperty("user-select");
+      document.removeEventListener("mousemove", mouseMoveHandler);
+      document.removeEventListener("mouseup", mouseUpHandler);
+    };
+    ele.addEventListener("mousedown", mouseDownHandler);
+  }
+});
