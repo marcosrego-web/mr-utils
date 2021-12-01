93fb5709606f948ba03769de21324fa98964655f
Marcos Rego <web@marcosrego.com>
2021-12-01 17:45:11 +0000

    Removed empty mr-

--- cms/wordpress/build/index.asset.php
+++ cms/wordpress/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '68a0b79b8f76e550c48ed847aa9d6642');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-components', 'wp-compose', 'wp-element', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'aeee29a70387f2afdfa9a1f248ade957');
